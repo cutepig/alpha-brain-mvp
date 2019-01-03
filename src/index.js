c9e231559de3e465ff5c582a522165e553b04e5c
Alex Seidler <sopo.porsas@gmail.com>
2019-01-03 14:26:02 +0200

    MVP is done
    - Add fullscreen
    
    - Add nosleep
    
    - Fix few issues

--- src/index.js
+++ src/index.js
@@ -3,60 +3,27 @@
 function getConfig() {
   return {
     visualModulationFrequency: 10,
-    visualHue: 0.5,
-    visualIntensity: 0.5,
+    visualHue: 180,   // From 0-360
+    visualIntensity: 50,
     audioCarrierFrequency: 100,
     audioModulationFrequency: 10,
   };
 }
 
-/**
- * Converts an HSL color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   Number  h       The hue
- * @param   Number  s       The saturation
- * @param   Number  l       The lightness
- * @return  Array           The RGB representation
- */
-function hslToRgb(h, s, l) {
-  var r, g, b;
-
-  if (s == 0) {
-    r = g = b = l; // achromatic
-  } else {
-    function hue2rgb(p, q, t) {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    }
-
-    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    var p = 2 * l - q;
-
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
-  }
-
-  return [r * 255, g * 255, b * 255];
-}
-
 class AlphaBrainScreen {
-  constructor(config, onStop) {
+  constructor({ config, onStop, nosleep }) {
     this.config = config;
+    this.nosleep = nosleep;
 
     this.el = redom.el('div.alpha-brain', { onclick: onStop });
-    this.el.style.setProperty('--visual-color', `rgb(${hslToRgb(config.visualHue, 1, config.visualIntensity)})`);
+    this.el.style.setProperty('--visual-color', `hsl(${config.visualHue}deg, 100%, ${config.visualIntensity}%)`);
     this.el.style.setProperty('--visual-modulation-frequency', `${0.5 / config.visualModulationFrequency}s`);
   }
 
   onmount() {
+    this.el.requestFullscreen();
+    this.nosleep.enable();
+
     const audioContext = new AudioContext;
     const carrierNode = audioContext.createOscillator();
     const modulatorNode = audioContext.createOscillator();
@@ -80,6 +47,12 @@ class AlphaBrainScreen {
   }
 
   onunmount() {
+    this.nosleep.disable();
+
+    if (document.fullscreenElement) {
+      document.exitFullscreen()
+    }
+
     this.gainNode.disconnect();
     this.carrierNode.stop();
     this.modulatorNode.stop();
@@ -95,6 +68,7 @@ function StartScreen(onStart) {
 class App {
   constructor() {
     this.config = getConfig();
+    this.nosleep = new NoSleep();
     this.screen = 'start'; // 'start' | 'alpha-brain'
     this.el = redom.el('main.app', this.renderChildren());
   }
@@ -107,7 +81,11 @@ class App {
     if (this.screen === 'start') {
       return StartScreen(() => this.onStart());
     } else {
-      return new AlphaBrainScreen(this.config, () => this.onStop());
+      return new AlphaBrainScreen({
+        config: this.config,
+        onStop: () => this.onStop(),
+        nosleep: this.nosleep
+      });
     }
   }
 
